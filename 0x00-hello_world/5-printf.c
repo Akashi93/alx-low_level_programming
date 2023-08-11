c93a08b6cf5e81e72d0f273596be371a1363d3de
Akashi93 <e-hab.93@live.com>
2023-08-11 07:00:47 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,11 @@
-/* first printf */
 #include <stdio.h>
-	int main(void)
+
+/**
+ * main - entry point
+ * description: prints a string
+ * Return: always (0) success
+ */
+int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 }
