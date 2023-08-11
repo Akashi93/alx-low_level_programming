56caed94d54aa24496d3356ba7d469dc15133710
Akashi93 <e-hab.93@live.com>
2023-08-11 07:07:41 -0700

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * description: puts a string
+ * Return: (0) success
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+}
