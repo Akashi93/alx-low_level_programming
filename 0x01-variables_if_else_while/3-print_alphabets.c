0ed91a818ef3a8419195210d0e0f444cf35e3a26
Akashi93 <e-hab.93@live.com>
2023-08-14 15:01:20 -0700

    print_alphabet

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -10,11 +10,14 @@ int main(void)
 
 {
 	char c;
-	for (c = 'a'; c <= 'z'; c++);
-		putchar'c';
-	for (c = 'A'; c <= 'Z'; c++);
-		putchar'c';
-	putchar(10)
+
+	{
+	for (c = 'a'; c <= 'z'; c++)
+		putchar('c');
+	for (c = 'A'; c <= 'Z'; c++)
+		putchar('c');
+	}
+	putchar(10);
 
 	return (0);
 }
