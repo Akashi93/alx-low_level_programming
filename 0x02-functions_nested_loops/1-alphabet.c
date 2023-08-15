277dd33b1f4e30be63d1775c354c149414d58563
Akashi93 <e-hab.93@live.com>
2023-08-15 15:10:45 -0700

    1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,11 +6,11 @@
 *Return: 0 (Success)
 */
 
-void print_alphabet(void)
+void print_alphabet(void);
 {
 	char a;
 
 	for (a = 'a'; a <= 'z'; a++)
 		_putchar(a);
-	_putchar("\n");
+		_putchar("\n");
 }
