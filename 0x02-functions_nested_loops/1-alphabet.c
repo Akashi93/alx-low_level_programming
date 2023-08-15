8e23c27321f5e797fa848ed78008a44408d0d2c0
Akashi93 <e-hab.93@live.com>
2023-08-15 15:53:40 -0700

    a

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,16 +2,16 @@
 
 /*
  * print_alphabet - prints the alphabet in lowercase
- *
+ *Description: Prints ALphabet in Lower case
  * Return: 0 on success
  */
 void print_alphabet(void)
 {
-    char a;
+	char a;
 
-    for (a = 'a'; a <= 'z'; a++)
-    {
-        _putchar(a);
-    }
-    _putchar('\n');
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		_putchar(a);
+	}
+	_putchar('\n');
 }
