2aab4bd794637822ffa56d07f38dc78d6697521a
Akashi93 <e-hab.93@live.com>
2023-08-14 17:14:43 -0700

    single_digit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Description: print single digit numbers
+ * Return: 0 (Successful)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+	printf("\n");
+	return (0);
+}
+
