5e13fb11159a36fc002df807275280cfcd5bdac3
Akashi93 <e-hab.93@live.com>
2023-08-16 05:05:50 -0700

    lower_case

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+*_islower - Prints lower case alphabet
+*@c: the character to check
+*Description: Print lower case alphabets
+*Return: 1 if lower case else return 0
+*/
+
+int _islower(int c)
+{
+	int c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		return (1);
+		return (0);
+}
