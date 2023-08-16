545cf4a89f307778acc21e66159a019da85bacf8
Akashi93 <e-hab.93@live.com>
2023-08-16 15:54:51 -0700

    qq

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-*isalpha - Prints the alphabet
+*_isalpha - Prints the alphabet
 *Description: Printd alphabet upper and lower case
 *@c: The character to print
 *Return: 1 if alphabet else prints 0
@@ -9,10 +9,9 @@
 
 int _isalpha(int c)
 {
-
-	for ((c = 'a' && c <= 'z') || (c = 'A' && c <= 'Z'))
-	{
+	if (c >= 'a' && c <= 'z')
 		return (1);
-	}
+	if (c >= 'A' && c <= 'Z')
+		return (1);
 	return (0);
 }
