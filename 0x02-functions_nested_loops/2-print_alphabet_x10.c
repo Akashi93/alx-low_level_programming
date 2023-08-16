3cd9b74bdbb192fcdcf27f4b9324e6ca7a346c64
Akashi93 <e-hab.93@live.com>
2023-08-16 02:34:28 -0700

    10x

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+*print_alphabet - Prints the alphabet
+*Description: Prints the alphabet 10xtimes
+*Return: void
+*/
+
+void print_alphabet(void)
+{
+	char a;
+	int i;
+
+	for  (i = 0; i <= 9; i++)
+	{
+	for (a = 'a'; a <= 'z'; a++)
+		_putchar(a);
+		puchar('\n')
+	{
+}
