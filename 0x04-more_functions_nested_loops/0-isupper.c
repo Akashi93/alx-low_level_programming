e9c4391fd4ca3752e3a94f6ab4828f2e03c67879
Akashi93 <e-hab.93@live.com>
2023-08-17 12:01:03 -0700

    isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+*_isupper - Checks for uppercase
+*@c: character to be checked
+*Description: Checks for uppercase characters
+*Return: 1 if uppercase else 0
+*/
+
+int _isupper(int c)
+{
+	int c;
+
+	while (c = 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+
+	return (0);
+}
