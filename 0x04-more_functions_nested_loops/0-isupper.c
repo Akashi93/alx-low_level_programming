37ac747269a9676b655af09f997a5de1c4e19cc6
Akashi93 <e-hab.93@live.com>
2023-08-17 18:24:49 -0700

    q

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,7 +2,6 @@
 
 /**
 *_isupper - Checks for uppercase
-*Description: Checks for uppercase characters
 *@c: Character checked
 *Return: 1 if uppercase else 0
 */
@@ -10,7 +9,8 @@
 int _isupper(int c)
 {
 	if (c == 65 && c <= 90)
+	{
 		return (1);
-	else
-		return (0);
+	}
+	return (0);
 }
